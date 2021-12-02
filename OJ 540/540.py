7e7f04e287b97a78a4cdf34ff0e3b5ec02a9fc5e
fam1709 <menescalfelipe34@gmail.com>
2021-12-02 14:31:29 -0300

    Problema 540 do Online Judge

--- OJ 540/540.py
+++ OJ 540/540.py
@@ -13,34 +13,41 @@ def main():
 		if(num_times == 0):
 			break
 		dict_times = {}
+		lista_chaves = []
 		for i in range(num_times):
-			dict_times[i] = entrada.pop(0)
+			dict_times[i] = entrada.pop(0).split()[1:]
 		print('Scenario #' + str(casos))
 		for linha in entrada:
 			if(linha == 'STOP'):
-				a = entrada.index(linha)
 				entrada = entrada[entrada.index(linha) + 1:len(entrada)]
 				casos = casos + 1
 				print('')
 				break
 			if('ENQUEUE' in linha):
 				elem = linha.split()[1]
+				for k,v in dict_times.items():
+					if elem in v:
+						chave_elem = k
+						break
 				if not fila:
 					fila.append(elem)
+					lista_chaves.append(chave_elem)
 				else:
-					pos_ult_membro = 0
-					for j in range(len(fila)):
-						possivel_membro = fila[j]
-						for k in range(len(dict_times)):
-							if (elem in list(dict_times.items())[k][1].split()[1:] and possivel_membro in list(dict_times.items())[k][1].split()[1:]):
-								pos_ult_membro = j + 1 
-								break
-					if(pos_ult_membro  == 0):
-						fila.append(elem)
+					if chave_elem in lista_chaves:
+						pos = lista_chaves[::-1].index(chave_elem)
+						if pos == 0:
+							fila.append(elem)
+							lista_chaves.append(chave_elem)
+						else:
+							nova_pos = len(lista_chaves) - pos
+							fila.insert(nova_pos,elem)
+							lista_chaves.insert(nova_pos,chave_elem)
 					else:
-						fila.insert(pos_ult_membro, elem)
+						lista_chaves.append(chave_elem)
+						fila.append(elem)
 			if('DEQUEUE' in linha):
 				head = fila.pop(0)
+				lista_chaves.pop(0)
 				print(head)
 
 if __name__ == '__main__':
